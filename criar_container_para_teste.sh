0eddc3597a772dc22ecb811f030654ac04771c7d
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-09-17 22:08:55 -0300

    ci: :sparkles: Criando Container PostGress
    Criando um container Postgress e sentando as configurações de porta e acesso

---
+++ criar_container_para_teste.sh
@@ -0,0 +1,8 @@
+# Criar e executar um container para um banco de dados PostgreSQL
+docker run -p 5432:5432 -d \
+-e POSTGRES_USER="dhionathanFIAP" \
+-e POSTGRES_PASSWORD="dhionathanFIAP_PASSWORD" \
+-e POSTGRES_DB="dhionathan_fiap_db" \
+-- name PostgressContainerDhionathanFiap \
+postgres
+ 
