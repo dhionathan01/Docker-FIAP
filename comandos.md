15ffd6f5e7bc8a1103213587da6a0fed365f36c8
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-09-17 22:41:49 -0300

    feat: :sparkles: Subindo Script Python que vai conectar no docker

---
+++ comandos.md
@@ -0,0 +1,4 @@
+#### Comandos Após criação do container 
+- Obter o IP da máquina que está rodando a imagem.
+```docker inspect postgresFiapTeste ```  
+- Resultado: ```"IPAddress": "172.17.0.3",```
---
+++ python_script_para_test.py
@@ -0,0 +1,16 @@
+from psycopg2 import connect
+conn = connect(
+    dbname = "dhionathan_fiap_db",
+    user = "dhionathanFIAP",
+    host = "172.17.0.3",
+    password = "dhionathanFIAP_PASSWORD"
+)
+cursor = conn.cursor()
+cursor.execute(f"SELECT * FROM my_table;")
+
+for i, record in enumerate(cursor);
+    print("\n", type(record))
+    print(record)
+
+cursor.close()
+conn.close()
