a159e2aa90197a73b891356a0f3dcfcf69ae2487
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-09-17 22:31:36 -0300

    build: :recycle: Rename

--- criar_container_para_teste.sh
+++ criar_container_para_teste.sh
@@ -3,6 +3,6 @@ docker run -p 5432:5432 -d \
 -e POSTGRES_USER="dhionathanFIAP" \
 -e POSTGRES_PASSWORD="dhionathanFIAP_PASSWORD" \
 -e POSTGRES_DB="dhionathan_fiap_db" \
--- name PostgressContainerDhionathanFiap \
+--name postgresFiapTeste \
 postgres
  
